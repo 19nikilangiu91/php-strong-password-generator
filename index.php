fd16eedf34b095dc5969b196a03ddf921e48571f
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-11 23:05:15 +0100

    Added styles

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Php Strong Password Generator</title>
+  <style>
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 0 auto;
+      width: 500px;
+      height: 250px;
+      border: 1px solid black;
+    }
+  </style>
   <?php
 
   session_start();
@@ -16,29 +28,32 @@
 </head>
 
 <body>
-  <h1>Strong Password Generator</h1>
-  <h2>Genera una password sicura</h2>
-  <form method="GET">
-    <label for="password">Lunghezza password:</label>
-    <input type="number" name="lenght">
-    <input type="submit" value="invia">
-    <h4>
-      <?php
-      $lenght = $_GET["lenght"] ?? false;
-      if ($lenght === false) {
-        echo " ";
-      } elseif ($lenght < 10) {
-        echo "Devi inserire almeno 10 caratteri";
-        // Milestone 3
-        // Invece di visualizzare la password nella `index.php`, effettuare un *redirect* ad una pagina dedicata che tramite `$_SESSION` recupererà la password da mostrare all’utente.
-      } else {
-        $show_password = password_generate($lenght);
-        $_SESSION["password"] = $show_password;
-        header('Location: ./password.php');
-      }
-      ?>
-    </h4>
-  </form>
+  <div class="container">
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
+    <form method="GET">
+      <label for="password">Lunghezza password:</label>
+      <input type="number" name="lenght">
+      <input type="submit" value="invia">
+      <h4>
+        <?php
+        $lenght = $_GET["lenght"] ?? false;
+        if ($lenght === false) {
+          echo " ";
+        } elseif ($lenght < 10) {
+          echo "Devi inserire almeno 10 caratteri";
+          // Milestone 3
+          // Invece di visualizzare la password nella `index.php`, effettuare un *redirect* ad una pagina dedicata che tramite `$_SESSION` recupererà la password da mostrare all’utente.
+        } else {
+          $show_password = password_generate($lenght);
+          $_SESSION["password"] = $show_password;
+          header('Location: ./password.php');
+        }
+        ?>
+      </h4>
+    </form>
+  </div>
+
 
 </body>
 
--- password.php
+++ password.php
@@ -6,17 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+            width: 500px;
+            height: 250px;
+            border: 1px solid black;
+        }
+    </style>
     <?php
     session_start();
     ?>
 </head>
 
 <body>
-    <h1>
-        <?php
-        echo "la tua password è: " . $_SESSION["password"];
-        ?>
-    </h1>
+    <div class="container">
+        <h1>
+            <?php
+            echo "la tua password è: " . $_SESSION["password"];
+            ?>
+        </h1>
+    </div>
 </body>
 
 </html>
