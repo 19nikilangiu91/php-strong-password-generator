a7110940dbfac5e6d9fea16617ef5846c9e531d3
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-11 14:54:42 +0100

    Update Milestone 2

--- index.php
+++ index.php
@@ -8,24 +8,15 @@
   <title>Php Strong Password Generator</title>
   <?php
 
-  $lenght = $_GET["lenght"] ?? false;
-  if ($lenght === false) {
-    echo " ";
-  } elseif ($lenght < 10) {
-    echo "Devi inserire almeno 10 caratteri";
-  } else
-    echo "La password generata è: " . password_generate($lenght);
+  require_once __DIR__ . "/libs/helper.php";
 
-
-  function password_generate($lenght)
-  {
-    $chars = array_merge(range("A", "Z"), range("a", "z"), range(0, 9), array("!", "@", "$", "%", "^", "&", "*"));
-    $password = "";
-    for ($i = 0; $i < $lenght; $i++) {
-      $password .= $chars[array_rand($chars)];
-    }
-    return $password;
-  }
+  // $lenght = $_GET["lenght"] ?? false;
+  // if ($lenght === false) {
+  //   echo " ";
+  // } elseif ($lenght < 10) {
+  //   echo "Devi inserire almeno 10 caratteri";
+  // } else
+  //   echo "La password generata è: " . password_generate($lenght);
   ?>
 </head>
 
@@ -36,6 +27,11 @@
     <label for="password">Lunghezza password:</label>
     <input type="number" name="lenght">
     <input type="submit" value="invia">
+    <h4>
+      <?php
+      echo getHtmlValidation($lenght);
+      ?>
+    </h4>
   </form>
 
 </body>
